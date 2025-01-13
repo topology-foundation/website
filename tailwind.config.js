8544f26daa8e2e97c00744f4f8d665f5d14d70f9
droak <me+git@droak.sh>
2025-01-13 09:41:38 +0530

    add new interim website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js,ts}"],
-  theme: {
-    extend: {
-      keyframes: {
-        scarf: {
-          "0%, 100%": {
-            transform: "rotate(1deg)",
-            "animation-timing-function": "cubic-bezier(0.5, 0, 0.5, 1)",
-          },
-          "50%": {
-            transform: "rotate(0)",
-            "animation-timing-function": "cubic-bezier(0.5, 0, 0.5, 1)",
-          },
-        },
-      },
-      animation: {
-        scarf: "scarf 8s infinite",
-      },
-    },
-    colors: {
-      "topology-border": "#77616C",
-      "topology-bg": "#FEFAF8",
-      "topology-scroll-bg": "#FCF2E8",
-      "topology-scroll-slider": "#E09C85",
-      "topology-red": "#E09C85",
-      "topology-green": "#D6D0A0",
-    },
-  },
-  plugins: [],
+	content: ["./public/index.html", "./src/**/*.{html,js,ts}"],
+	theme: {},
+	plugins: [],
 };
